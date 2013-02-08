iNode/mc

commit 68468a25acea04c44320d0a75226a051d7c2b64a

Ticket #2601: incorrect TTY layer initialization.
The xterm_flag variable was initialized in setup_mc() but used
first time in init_key() and in tty_init() (in do_enter_ca_mode())
before setup_mc() call.

Now xterm initialized in early step of mc start up process and
xterm support and mouse are initialized in tty_init().

Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

lib/tty/tty-ncurses.c
@@ -46,6 +46,7 @@
 #include "tty-internal.h"       /* slow_tty */
 #include "tty.h"
 #include "color-internal.h"
+#include "mouse.h"
 #include "win.h"
 
 /* include at last !!! */
@@ -137,7 +138,7 @@ mc_tty_normalize_lines_char (const char *ch)
 /* --------------------------------------------------------------------------------------------- */
 
 void
-tty_init (gboolean slow, gboolean ugly_lines)
+tty_init (gboolean slow, gboolean ugly_lines, gboolean mouse_enable, gboolean is_xterm)
 {
     slow_tty = slow;
     (void) ugly_lines;
@@ -165,6 +166,10 @@ tty_init (gboolean slow, gboolean ugly_lines)
 
     tty_start_interrupt_key ();
 
+    if (!mouse_enable)
+        use_mouse_p = MOUSE_DISABLED;
+    tty_init_xterm_support (is_xterm);  /* do it before do_enter_ca_mode() call */
+    init_mouse ();
     do_enter_ca_mode ();
     tty_raw_mode ();
     noecho ();

lib/tty/tty-slang.c
@@ -243,14 +243,14 @@ mc_tty_normalize_lines_char (const char *str)
 /* --------------------------------------------------------------------------------------------- */
 
 void
-tty_init (gboolean slow, gboolean ugly_lines)
+tty_init (gboolean slow, gboolean ugly_lines, gboolean mouse_enable, gboolean is_xterm)
 {
     slow_tty = slow;
     ugly_line_drawing = ugly_lines;
 
     SLtt_Ignore_Beep = 1;
 
-    SLutf8_enable (-1); /* has to be called first before any of the other functions. */
+    SLutf8_enable (-1);         /* has to be called first before any of the other functions. */
     SLtt_get_terminfo ();
     /*
      * If the terminal in not in terminfo but begins with a well-known
@@ -303,7 +303,18 @@ tty_init (gboolean slow, gboolean ugly_lines)
     /* It's the small part from the previous init_key() */
     init_key_input_fd ();
 
+    /* For 8-bit locales, NCurses handles 154 (0x9A) symbol properly, while S-Lang
+     * requires SLsmg_Display_Eight_Bit >= 154 (OR manual filtering if xterm display
+     * detected - but checking TERM would fail under screen, OR running xterm
+     * with allowC1Printable).
+     */
+    tty_display_8bit (FALSE);
+
     SLsmg_init_smg ();
+    if (!mouse_enable)
+        use_mouse_p = MOUSE_DISABLED;
+    tty_init_xterm_support (is_xterm);  /* do it before do_enter_ca_mode() call */
+    init_mouse ();
     do_enter_ca_mode ();
     tty_keypad (TRUE);
     tty_nodelay (FALSE);

src/main.c
@@ -411,6 +411,13 @@ main (int argc, char *argv[])
     if (!mc_args_handle (argc, argv, "mc"))
         exit (EXIT_FAILURE);
 
+    /* check terminal type
+     * $TEMR must be set and not empty
+     * xterm_flag is used in init_key() and tty_init()
+     * Do this after mc_args_handle() where mc_args__force_xterm is set up.
+     */
+    xterm_flag = tty_check_term (mc_args__force_xterm);
+
     /* NOTE: This has to be called before tty_init or whatever routine
        calls any define_sequence */
     init_key ();
@@ -435,7 +442,8 @@ main (int argc, char *argv[])
 
     /* Must be done before init_subshell, to set up the terminal size: */
     /* FIXME: Should be removed and LINES and COLS computed on subshell */
-    tty_init (mc_global.args.slow_terminal, mc_global.args.ugly_line_drawing);
+    tty_init (mc_global.args.slow_terminal, mc_global.args.ugly_line_drawing, !mc_args__nomouse,
+              xterm_flag);
 
     load_setup ();
 

lib/tty/tty-internal.h
@@ -39,6 +39,7 @@ extern gboolean mouse_enabled;
 /*** declarations of public functions ************************************************************/
 
 char *mc_tty_normalize_from_utf8 (const char *);
+void tty_init_xterm_support (gboolean is_xterm);
 
 /*** inline functions ****************************************************************************/
 #endif /* MC_TTY_INTERNAL_H */

lib/tty/tty.c
@@ -33,6 +33,8 @@
 
 #include <signal.h>
 #include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>     /* exit() */
 
 #ifdef HAVE_SYS_IOCTL_H
 #include <sys/ioctl.h>
@@ -43,6 +45,7 @@
 
 #include "tty.h"
 #include "tty-internal.h"
+#include "mouse.h"      /* use_mouse_p */
 #include "win.h"
 
 /*** global variables ****************************************************************************/
@@ -80,6 +83,34 @@ sigintr_handler (int signo)
 
 /* --------------------------------------------------------------------------------------------- */
 /*** public functions ****************************************************************************/
+/* --------------------------------------------------------------------------------------------- */
+
+/**
+ * Check terminal type. If $TERM is not set or value is empty, mc finishes with EXIT_FAILURE.
+ *
+ * @param force_xterm Set forced the XTerm type
+ *
+ * @return true if @param force_xterm is true or value of $TERM is one of term*, konsole*
+ *              rxvt*, Eterm or dtterm
+ */
+gboolean
+tty_check_term (gboolean force_xterm)
+{
+    const char *termvalue;
+
+    termvalue = getenv ("TERM");
+    if (termvalue == NULL || *termvalue == '\0')
+    {
+        fputs (_("The TERM environment variable is unset!\n"), stderr);
+        exit (EXIT_FAILURE);
+    }
+
+    return force_xterm || strncmp (termvalue, "xterm", 5) == 0
+        || strncmp (termvalue, "konsole", 7) == 0
+        || strncmp (termvalue, "rxvt", 4) == 0
+        || strcmp (termvalue, "Eterm") == 0 || strcmp (termvalue, "dtterm") == 0;
+}
+
 /* --------------------------------------------------------------------------------------------- */
 
 extern gboolean
@@ -256,6 +287,44 @@ tty_low_level_change_screen_size (void)
     }
 #endif /* TIOCGWINSZ */
 #endif /* defined(HAVE_SLANG) || NCURSES_VERSION_MAJOR >= 4 */
+}
+
+/* --------------------------------------------------------------------------------------------- */
+
+void
+tty_init_xterm_support (gboolean is_xterm)
+{
+    const char *termvalue;
+
+    termvalue = getenv ("TERM");
+
+    /* Check mouse capabilities */
+    xmouse_seq = tty_tgetstr ("Km");
+
+    if (strcmp (termvalue, "cygwin") == 0)
+    {
+        is_xterm = TRUE;
+        use_mouse_p = MOUSE_DISABLED;
+    }
+
+    if (is_xterm)
+    {
+        /* Default to the standard xterm sequence */
+        if (xmouse_seq == NULL)
+            xmouse_seq = ESC_STR "[M";
+
+        /* Enable mouse unless explicitly disabled by --nomouse */
+        if (use_mouse_p != MOUSE_DISABLED)
+        {
+            const char *color_term = getenv ("COLORTERM");
+            if (strncmp (termvalue, "rxvt", 4) == 0 ||
+                (color_term != NULL && strncmp (color_term, "rxvt", 4) == 0) ||
+                strcmp (termvalue, "Eterm") == 0)
+                use_mouse_p = MOUSE_XTERM_NORMAL_TRACKING;
+            else
+                use_mouse_p = MOUSE_XTERM_BUTTON_EVENT_TRACKING;
+        }
+    }
 }
 
 /* --------------------------------------------------------------------------------------------- */

lib/tty/tty.h
@@ -75,7 +75,9 @@ extern void tty_beep (void);
 
 /* {{{ Input }}} */
 
-extern void tty_init (gboolean slow, gboolean ugly_lines);
+extern gboolean tty_check_term (gboolean force_xterm);
+extern void tty_init (gboolean slow, gboolean ugly_lines, gboolean mouse_enable,
+                      gboolean is_xterm);
 extern void tty_shutdown (void);
 
 extern gboolean tty_is_slow (void);

lib/tty/win.c
@@ -41,7 +41,7 @@
 
 /* This flag is set by xterm detection routine in function main() */
 /* It is used by function view_other_cmd() */
-int xterm_flag = 0;
+gboolean xterm_flag = FALSE;
 
 /*** file scope macro definitions ****************************************************************/
 

lib/tty/win.h
@@ -15,7 +15,7 @@
 
 /*** global variables defined in .c file *********************************************************/
 
-extern int xterm_flag;
+extern gboolean xterm_flag;
 
 /*** declarations of public functions ************************************************************/
 

commit a98622439d39c83264ed58898cf531f9e43ad24e

Ticket #2964: mouse doesn't select text in subshell in native console.
Steps to Reproduce:

1. Run mc in native console (not in X terminal emulator).
2. Press Ctrl+O to switch to subshell.
3. Try select anything with mouse.
Result: mouse does't select anything.

This bug was introduced in 68468a25acea04c44320d0a75226a051d7c2b64a
commit.

Solution: make mouse initialization after initializaton of subshell.

Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

lib/tty/tty-ncurses.c
@@ -203,7 +203,6 @@ tty_init (gboolean mouse_enable, gboolean is_xterm)
     if (!mouse_enable)
         use_mouse_p = MOUSE_DISABLED;
     tty_init_xterm_support (is_xterm);  /* do it before do_enter_ca_mode() call */
-    init_mouse ();
     do_enter_ca_mode ();
     tty_raw_mode ();
     noecho ();

lib/tty/tty-slang.c
@@ -332,7 +332,6 @@ tty_init (gboolean mouse_enable, gboolean is_xterm)
     if (!mouse_enable)
         use_mouse_p = MOUSE_DISABLED;
     tty_init_xterm_support (is_xterm);  /* do it before do_enter_ca_mode() call */
-    init_mouse ();
     do_enter_ca_mode ();
     tty_keypad (TRUE);
     tty_nodelay (FALSE);

src/main.c
@@ -47,6 +47,7 @@
 #include "lib/event.h"
 #include "lib/tty/tty.h"
 #include "lib/tty/key.h"        /* For init_key() */
+#include "lib/tty/mouse.h"      /* init_mouse() */
 #include "lib/skin.h"
 #include "lib/filehighlight.h"
 #include "lib/fileloc.h"
@@ -367,7 +368,6 @@ main (int argc, char *argv[])
     /* inherit the file descriptors opened below, etc */
     if (mc_global.tty.use_subshell)
         init_subshell ();
-
 #endif /* ENABLE_SUBSHELL */
 
     /* Also done after init_subshell, to save any shell init file messages */
@@ -385,6 +385,10 @@ main (int argc, char *argv[])
         message (D_ERROR, _("Warning"), "%s", config_migrate_msg);
         g_free (config_migrate_msg);
     }
+
+    /* Done after subshell initialization to allow select and paste text by mouse
+       w/o Shift button in subshell in the native console */
+    init_mouse ();
 
     /* Program main loop */
     if (mc_global.midnight_shutdown)